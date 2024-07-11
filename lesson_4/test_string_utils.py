463d163c4bbd4e7c80d7d54f11d6d1a8384eeddb
Artur <156789062+ArturMavl@users.noreply.github.com>
2024-07-11 09:20:04 +0500

    4

--- lesson_4/test_string_utils.py
+++ lesson_4/test_string_utils.py
@@ -5,71 +5,71 @@ utils = StringUtils()
 
 
 def test_capitalize():
-    res = uti.capitalize ("привет")
+    res = utils.capitalize ("привет")
     assert res == "Привет"
 def test_capitalize_1():
-    res = uti.capitalize("ПРИВЕТ")
+    res = utils.capitalize("ПРИВЕТ")
     assert res == "Привет"
 def test_trim():
-    res = uti.trim(" привет")
+    res = utils.trim(" привет")
     assert res == "привет"
 def test_trim_1():
-    res = uti.trim ("при вет")
+    res = utils.trim ("при вет")
     assert res == "при вет"
 def test_to_list():
-    res = uti.to_list("1/2/3/4/5", "/")
+    res = utils.to_list("1/2/3/4/5", "/")
     assert res == ["1", "2", "3", "4", "5" ]
 def test_to_list_1():
-    res = uti.to_list("", "/")
+    res = utils.to_list("", "/")
     assert res == []
 def test_to_list_2():
-    res = uti.to_list("1/2/3/4/5")
+    res = utils.to_list("1/2/3/4/5")
     assert res == ["1/2/3/4/5"]
 def test_contains():
-    res = uti.contains("Привет", "П")
+    res = utils.contains("Привет", "П")
     assert res == True
 def test_contains_1():
-    res = uti.contains("Привет", "ф")
+    res = utils.contains("Привет", "ф")
     assert res == False
 def test_delete_symbol():
-    res = uti.delete_symbol("Привет", "в")
+    res = utils.delete_symbol("Привет", "в")
     assert res == "Приет"
 def test_delete_symbol_1():
-    res = uti.delete_symbol("Привет", "о")
+    res = utils.delete_symbol("Привет", "о")
     assert res == "Привет"
 def test_delete_symbol_2():
-    res = uti.delete_symbol("12", "o")
+    res = utils.delete_symbol("12", "o")
     assert res == "12"
 def test_starts_with():
-    res = uti.starts_with("Привет", "П")
+    res = utils.starts_with("Привет", "П")
     assert res == True
 def test_starts_with_1():
-    res = uti.starts_with("Привет", "п")
+    res = utils.starts_with("Привет", "п")
     assert res == False
 def test_starts_with_2():
-    res = uti.starts_with("Привет", "Н")
+    res = utils.starts_with("Привет", "Н")
     assert res == False
 def test_end_with():
-    res = uti.end_with("Привет", "т")
+    res = utils.end_with("Привет", "т")
     assert res == True
 def test_end_with_1():
-    res = uti.end_with("Привет", "Т")
+    res = utils.end_with("Привет", "Т")
     assert res == False
 def test_end_with_2():
-    res = uti.end_with("Привет", "з")
+    res = utils.end_with("Привет", "з")
     assert res == False
 def test_is_empty():
-    res = uti.is_empty(" ")
+    res = utils.is_empty(" ")
     assert res == True
 def test_is_empty_1():
-    res = uti.is_empty("r")
+    res = utils.is_empty("r")
     assert res == False
 def test_is_empty_2():
-    res = uti.is_empty("")
+    res = utils.is_empty("")
     assert res == True
 def test_list_to_string():
-    res = uti.list_to_string[1, 2, 3, 4, 5],
+    res = utils.list_to_string[1, 2, 3, 4, 5],
     assert res == "1,2,3,4,5"
 def test_list_to_string_1():
-    res = uti.list_to_string([], ",")
+    res = utils.list_to_string([], ",")
     assert res == ""
