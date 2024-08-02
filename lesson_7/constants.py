4be45fc38502a51bbff66776402cb83bfe94a0a0
Artur <156789062+ArturMavl@users.noreply.github.com>
2024-08-02 20:28:26 +0500

    7

---
+++ lesson_7/constants.py
@@ -0,0 +1,6 @@
+# 1 задание
+Test_form_URL = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+# 2 задание
+Calculator_URL = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+# 3 задание
+Shop_URL = "https://www.saucedemo.com/"
